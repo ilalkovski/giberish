274bcf3c15a5e6f3e1a6283be0a8975b2b102763
Shiki <shiki@localhost.(none)>
2013-03-02 13:38:33 +0100

    Added stub train class WFGiberishTrain

---
+++ Giberish-core/WFGiberishTrain.h
@@ -0,0 +1,13 @@
+//
+//  WFGiberishTrain.h
+//  Giberish
+//
+//  Created by Shiki on 3/2/13.
+//  Copyright (c) 2013 webfactory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WFGiberishTrain : NSObject
+
+@end
---
+++ Giberish-core/WFGiberishTrain.m
@@ -0,0 +1,13 @@
+//
+//  WFGiberishTrain.m
+//  Giberish
+//
+//  Created by Shiki on 3/2/13.
+//  Copyright (c) 2013 webfactory. All rights reserved.
+//
+
+#import "WFGiberishTrain.h"
+
+@implementation WFGiberishTrain
+
+@end
